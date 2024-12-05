7fd80c0bf7a3cfdd0161b5f1829a8d75e253912d
Cyber Oliveira <lccro@msn.com>
2024-12-05 12:20:00 -0300

    day 5 refactoring

--- ruby/5.rb
+++ ruby/5.rb
@@ -5,37 +5,33 @@
 class P5
   attr_reader :part1, :part2
 
-  def initialize(filename)
+  def initialize(filename) # rubocop:disable Metrics/AbcSize,Metrics/CyclomaticComplexity,Metrics/MethodLength,Metrics/PerceivedComplexity
     rules, pages = File.read(filename).split("\n\n")
-    rules = rules.split("\n")
-                 .map { |l| l.split('|') }
-                 .map { |r| [r[0].to_i, r[1].to_i] }
-                 .group_by(&:first)
-                 .transform_values { |a| a.map(&:last) }
+    @rules = rules.split("\n")
+                  .map { |l| l.split('|') }
+                  .map { |r| [r[0].to_i, r[1].to_i] }
+                  .group_by(&:first)
+                  .transform_values { |a| a.map(&:last) }
     pages = pages
             .split("\n")
             .map { |l| l.split(',') }
             .map { |p| p.map(&:to_i) }
 
-    @part1 = pages
-             .filter { |p| valid?(p, rules) }
-             .map { |p| p[p.size / 2] }
-             .sum
-    @part2 = pages
-             .reject { |p| valid?(p, rules) }
-             .map { |p| p.sort { |a, b| rules[a]&.include?(b) ? 1 : -1 } }
-             .map { |p| p[p.size / 2] }
-             .sum
+    sum = ->(pp) { pp.map { |p| p[p.size / 2] }.sum }
+    sort = ->(pp) { pp.sort { |a, b| @rules[a]&.include?(b) ? 1 : -1 } }
+
+    @part1 = sum.call(pages.filter { |p| valid?(p) })
+    @part2 = sum.call(pages.reject { |p| valid?(p) }.map { |p| sort.call(p) })
   end
 
   private
 
-  def valid?(page, rules)
-    page.each.with_index do |p, i|
-      return false if rules.key?(p) \
-        && (page[...i].any? { |p2| rules[p].include?(p2) } || !page[i + 1..].all? { |p2| rules[p].include?(p2) })
+  def valid?(page)
+    page.each.with_index.none? do |p, i|
+      @rules.key?(p) \
+        && (page[...i].any? { |p2| @rules[p].include?(p2) } || \
+            !page[i + 1..].all? { |p2| @rules[p].include?(p2) })
     end
-    true
   end
 end
 
